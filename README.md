5048a4a63a93fcea7fa4d0192f6f01c3e9a6618f
peppapig13132 <peppapig13132@outlook.com>
2024-05-21 02:56:00 +0200

    Initialize React.js/TypeScrpt/TailwindCSS app

---
+++ README.md
@@ -0,0 +1,6 @@
+# STRIPE DASHBOARD (FE)
+
+## Environment
+
+- Node.js v20.12.2 (LTS)
+- npm v10.6.0
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-4xl my-3 text-slate-700">
+        Hello React.js!
+      </h2>
+      <p className="my-3 text-slate-700">
+        React.js boilerplate pre-configured with TypeScript and Tailwind CSS
+      </p>
+    </div>
+  );
+}
+
+export default App;
