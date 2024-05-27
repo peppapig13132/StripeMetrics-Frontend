38b1418857af2675b362ef684c39d3cd21417341
peppapig13132 <peppapig13132@outlook.com>
2024-05-28 01:39:25 +0200

    Bugfix: In MRR Movements chart, date/year of the tooltip is incorrect

--- src/components/mrr-movements/MrrMovementsChart.tsx
+++ src/components/mrr-movements/MrrMovementsChart.tsx
@@ -1,46 +1,54 @@
 import ReactECharts from 'echarts-for-react';
-import { formatDateString1, formatDateString2 } from '../../utils/functions';
+import { format } from "date-fns";
 
 interface ChartData {
   xData: string[];
   yData: number[];
 }
 
+export const formatDateString = (dateString: string): string => {
+  const date = new Date(dateString);
+  return format(date, 'MMM dd, yyyy');
+}
 const MrrMovementsChart = (data: ChartData) => {
 
-  const _tooltipFormatter = (item: any) => {
-    const date = formatDateString1(item[0].name);
-    const sum = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item[0].value);
+  const _tooltipFormatter = (params: any) => {
+    const date = formatDateString(params[0].name);
+    const sum = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(params[0].value);
+
     return `<div><span class="text-xs">${date}</span><br/><span></span>${sum}</div>`;
   };
 
-  const xData = data.xData.map((item) => formatDateString2(item));
+  const xData = data.xData.map((item) => formatDateString(item));
   const yData = data.yData;
 
   const option = {
     tooltip: {
       trigger: 'axis',
-      formatter: _tooltipFormatter
+      formatter: _tooltipFormatter,
     },
     xAxis: {
       type: 'category',
       data: xData,
+      axisLabel: {
+        formatter: (value: string) => value.substring(0, 6),
+      }
     },
     yAxis: {
-      type: 'value'
+      type: 'value',
     },
     series: [
       {
         data: yData,
         type: 'line',
-        smooth: true
+        smooth: true,
       }
     ],
     grid: {
 			right: '10px',
 			left: '30px',
 			bottom: '25px',
-			top: '25px'
+			top: '25px',
 		}
   };
 
