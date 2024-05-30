3c5fa46194dbfb433aa0f37da2469c812e772049
peppapig13132 <peppapig13132@outlook.com>
2024-05-31 00:00:35 +0200

    Logout button on Dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -10,6 +10,7 @@ import { CustomerChurnRate } from '../../components/customer-churn-rate/Customer
 import { FreeToPaidSubscriptions } from '../../components/free-to-paid-subscriptions/FreeToPaidSubscriptions';
 import { FreeTrials } from '../../components/free-trials/FreeTrials';
 import { AnnualRunRate } from '../../components/annual-run-rate/AnnualRunRate';
+import { useAuth } from '../../context/AuthContext';
 
 export const Dashboard: React.FC = () => {
   const [value, setValue] = useState({ 
@@ -22,16 +23,24 @@ export const Dashboard: React.FC = () => {
     setValue(newValue); 
   }
 
+  const { appLogout } = useAuth();
+
   return(
     <div className="bg-slate-200 w-full">
       <div className="dashboard container mx-auto w-full min-h-screen p-8">
         <div className="dashboard__header p-2 flex flex-col lg:flex-row">
           <h2 className="font-bold text-3xl text-sky-600">Dashboard</h2>
-          <div className="w-full lg:max-w-[275px] mt-3 lg:mt-0 ms-auto">
+          <div className="w-full lg:max-w-[300px] mt-3 lg:mt-0 ms-auto flex flex-row items-center justify-center align-center">
             <Datepicker
               value={value} 
               onChange={handleValueChange} 
             />
+
+            <button onClick={appLogout} className='bg-slate-300 hover:bg-sky-600 ms-3 px-2 py-2 text-sm leading-5 rounded-md font-semibold text-sky-600 hover:text-white border border-slate-300 hover:border-slate-300'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+              </svg>
+            </button>
           </div>
         </div>
 
