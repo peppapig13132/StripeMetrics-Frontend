71b633147f69de12c500fd26e7d29469373f0dfc
peppapig13132 <peppapig13132@outlook.com>
2024-06-07 17:14:15 +0200

    Update Annual Run Rate, get data in specific date range

--- src/components/annual-run-rate/AnnualRunRate.tsx
+++ src/components/annual-run-rate/AnnualRunRate.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import { getMrrData } from "../../services/dashboardApiService";
-import { DailySum } from "../../interfaces/interface";
 import { setAuthToken } from "../../utils/setAuthToken";
 import { useAuth } from "../../context/AuthContext";
 import { useDashboard } from "../../context/DashboardContext";
+import moment from "moment";
 
 export const AnnualRunRate = () => {
-  const [mrr30Days, setMrr30Days] = useState(0);
-  const [mrrLastMonth, setMrrLastMonth] = useState(0);
-  const [rate, setRate] = useState(0);
-  const [formattedAnnualRunRate30Days, setFormattedAnnualRunRate30Days] = useState('$0');
+  const [mrr, setMrr] = useState(0);
+  const [formattedAnnualRunRate, setFormattedAnnualRunRate] = useState('$0');
 
   const { token } = useAuth();
   const { dateRange } = useDashboard();
@@ -22,22 +20,10 @@ export const AnnualRunRate = () => {
         const response = await getMrrData(dateRange);
 
         if(response.ok) {
-          const mrr_array = response.mrr_array;
-          mrr_array.sort((a: DailySum, b: DailySum) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
-          
-          setMrr30Days(response.mrr_last_30days);
-          setMrrLastMonth(response.mrr_last_month);
-
-          if(mrrLastMonth !== 0) {
-            setRate(Math.round((mrr30Days - mrrLastMonth) / mrrLastMonth * 10000) / 100);
-          } else {
-            setRate(100);
-          }
-          setFormattedAnnualRunRate30Days(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(mrr30Days * 12));
+          setMrr(response.mrr);
+          setFormattedAnnualRunRate(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(mrr * 12));
         } else {
-          setMrr30Days(0);
-          setMrrLastMonth(0);
-          setRate(0);
+          setMrr(0);
         }
 
       } catch(error) {
@@ -46,18 +32,17 @@ export const AnnualRunRate = () => {
     }
 
     fetchData();
-  }, [token, mrr30Days, mrrLastMonth, dateRange]);
+  }, [token, mrr, dateRange]);
 
   return (
     <div className="bg-white w-full rounded-xl p-5">
       <div className="flex flex-row">
         <h4 className="font-bold text-sky-600">Annual Run Rate</h4>
-        <span className="font-bold text-red-500 ms-auto">{rate}%</span>
       </div>
 
       <div className="flex flex-row mt-8">
-        <span className="font-bold text-sky-600">{formattedAnnualRunRate30Days}</span>
-        <span className="text-sky-600 ms-auto">Last 30 days</span>
+        <span className="font-bold text-sky-600">{formattedAnnualRunRate}</span>
+        <span className="text-sky-600 ms-auto">{moment(dateRange.endDate).format('DD, MMM YYYY')}</span>
       </div>
     </div>
   );
